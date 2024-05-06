d26ff4e054a36cb4c3567adb3772a7f0d83f2c69
Hsen-123 <hazimehussein01@gmail.com>
2024-05-06 19:38:35 +0300

    fuck

--- src/app/page.js
+++ src/app/page.js
@@ -1,5 +1,4 @@
 "use client";
-import { useState, useEffect } from "react";
 import Followers from "@/components/Followers";
 import Hero from "@/components/Hero";
 import Services from "@/components/Services";
@@ -9,24 +8,14 @@ import Image from "next/image";
 import { InlineWidget } from "react-calendly";
 
 export default function Home() {
-  const [showFollowers, setShowFollowers] = useState(false);
-
-  useEffect(() => {
-    const timer = setTimeout(() => {
-      setShowFollowers(true);
-    }, 3000); // Wait for 3 seconds before showing Followers
-    return () => clearTimeout(timer); // Cleanup the timer to avoid memory leaks
-  }, []);
-
   if (typeof window === "undefined") {
     return null;
   }
-
   return (
     <div>
       <Hero />
       <Services />
-      {showFollowers && <Followers />}
+      <Followers />
       <Testimonials />
       <Videos />
       <div id="book">
