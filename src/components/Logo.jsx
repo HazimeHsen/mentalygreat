1085859cb9a0a60f514354ecf99b59ae0118e525
Hsen-123 <hazimehussein01@gmail.com>
2024-05-06 21:49:41 +0300

    change logo

--- src/components/Logo.jsx
+++ src/components/Logo.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 
 const Logo = () => {
-  return <div className="text-xl font-semibold underline">MentallyGreat</div>;
+  return <div className="text-xl font-semibold underline">MG Media</div>;
 };
 
 export default Logo;
