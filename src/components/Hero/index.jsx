d282b7fbb296d95f5272f9b94c0db997e33d3dcb
Hsen-123 <hazimehussein01@gmail.com>
2024-05-08 18:48:52 +0300

    changes

--- src/components/Hero/index.jsx
+++ src/components/Hero/index.jsx
@@ -3,22 +3,25 @@ import React from "react";
 import { FaFacebook, FaInstagram, FaTiktok, FaYoutube } from "react-icons/fa";
 import { motion } from "framer-motion";
 import Image from "next/image";
-// bg-gradient-to-r from-blue-600 to-white bg-clip-text text-transparent
+
+
 const Hero = () => {
   return (
     <div
-      className="bg-cover bg-top bg-no-repeat pt-20 md:pt-14 md:mb-0 mb-[520px] md:text-start text-center md:px-14 lg:px-20 px-5 flex justify-center items-center min-h-screen text-white"
+      className="bg-cover bg-top bg-no-repeat pt-20 md:pt-14 md:mb-0 mb-[590px] md:text-start text-center md:px-14 lg:px-20 px-5 flex justify-center items-center min-h-screen text-white"
       style={{
         backgroundImage: "url(/hero.png)",
       }}>
       <div className="absolute inset-0 bg-black opacity-50"></div>
 
-      <div className="hero-content md:justify-between justify-center md:relative md:top-0 md:left-0 absolute top-56 left-0 flex md:flex-row flex-col items-center w-full">
+      <div className="hero-content md:justify-between justify-center md:relative md:top-0 md:left-0 absolute top-64 left-0 flex md:flex-row flex-col items-center w-full">
         <div className="">
-          <h1 className="fade-in mb-5 text-3xl md:text-5xl font-bold ">
+          <h1 className="fade-in mb-5 text-3xl md:text-5xl font-bold verdana-font">
             Creating Viral Videos
           </h1>
-          <p className="max-w-lg fade-in mb-5 text-sm md:text-base">
+          <p
+            style={{ fontFamily: "Verdana", fontWeight: "bold" }}
+            className="max-w-lg fade-in mb-5 text-sm md:text-base ">
             Hey, I’m Fouad! In February 2021, I set a big bold goal: inspire 1
             million followers in 12 months. I had 8,000 followers at the time.
             But I said to myself: {'"'}You can{"'"}t miss out on this huge
