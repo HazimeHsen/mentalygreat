f5e53523730eee588c37df6a00eea71fa27ad2c8
Hsen-123 <hazimehussein01@gmail.com>
2024-05-12 21:00:19 +0300

    ,

--- src/components/PageLoading.jsx
+++ src/components/PageLoading.jsx
@@ -1,14 +1,13 @@
+import Image from "next/image";
 import React from "react";
+import Logo from "./Logo";
 
 export default function PageLoading() {
   return (
-    <div className="h-screen w-screen flex justify-center items-center">
-      <div
-        className="inline-block h-8 w-8 animate-spin rounded-full border-4 border-solid border-current border-r-transparent align-[-0.125em] motion-reduce:animate-[spin_1.5s_linear_infinite]"
-        role="status">
-        <span className="!absolute !-m-px !h-px !w-px !overflow-hidden !whitespace-nowrap !border-0 !p-0 ![clip:rect(0,0,0,0)]">
-          Loading...
-        </span>
+    <div className="flex-col h-screen w-screen relative gap-4 flex items-center justify-center">
+      <div className="w-20 h-20 border-8 text-accent text-4xl animate-spin border-gray-300 p-2 flex items-center justify-center border-t-black rounded-full"></div>
+      <div className="animate-pulse absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 p-2">
+        <Image src={"/logo.png"} width={50} height={50} alt="logo" />
       </div>
     </div>
   );
